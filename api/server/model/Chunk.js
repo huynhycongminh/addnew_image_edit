455844fb5bd1d25093c87e0b8c79368df3980f43
voduckhoa2810 <khoa28101998@gmail.com>
2021-12-25 13:10:49 +0700

    Update

---
+++ api/server/model/Chunk.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("./GFS");
+
+const Chunk = mongoose.model(
+  "Chunk",
+  new mongoose.Schema({
+    filename_id: {
+      type: String,
+      ref: "GFS",
+    },
+    data: Buffer,
+  }),
+  "fs.chunks"
+);
+
+module.exports = Chunk;
